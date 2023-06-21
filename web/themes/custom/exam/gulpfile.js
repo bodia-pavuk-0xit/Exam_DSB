bacc13fca8ee25becfcd78455eee83f7c671d08f
bodia-pavuk-0xit <b.yakobchuk.workmail@gmail.com>
2023-06-21 17:52:48 +0300

    Fixing bugs

--- web/themes/custom/exam/gulpfile.js
+++ web/themes/custom/exam/gulpfile.js
@@ -2,21 +2,17 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const rename = require('gulp-rename');
 
-gulp.task('styles', () => {
-  return gulp.src([
-    './scss/global.scss',
-    './scss/*.scss',
-  ])
+gulp.task('sass', function () {
+  return gulp.src('style/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .on('error', sass.logError)
-    .pipe(rename({dirname: ''}))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest(function (file) {
+      return file.base;
+    }));
 });
 
-gulp.task('watch', function() {
-  gulp.watch(['scss/*', 'Components/**/*'], { ignoreInitial: false }, gulp.series(['styles']));
+gulp.task('watch', function () {
+  gulp.watch('style/*.scss', gulp.series('sass'));
 });
 
-gulp.task('default', gulp.series(['styles', 'watch']));
+gulp.task('default', gulp.series('sass', 'watch'));
